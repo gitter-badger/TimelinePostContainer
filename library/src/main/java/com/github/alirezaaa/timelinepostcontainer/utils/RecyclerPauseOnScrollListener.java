b2e8c336eb10f95f608436459b7ebba13b18f388
Alireza <aesshoferi@gmail.com>
2016-03-06 20:05:41 +0330

    Avoid scrolling lags utils

--- library/src/main/java/com/github/alirezaaa/timelinepostcontainer/utils/RecyclerPauseOnScrollListener.java
+++ library/src/main/java/com/github/alirezaaa/timelinepostcontainer/utils/RecyclerPauseOnScrollListener.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.github.alirezaaa.timelinepostcontainer.app;
+package com.github.alirezaaa.timelinepostcontainer.utils;
 
 import android.support.v7.widget.RecyclerView;
 
