bdf32139f18671fa25476f400dd4806a00d14090
Alireza <aesshoferi@gmail.com>
2016-03-06 20:37:28 +0330

    removed unused util method

--- library/src/main/java/com/github/alirezaaa/timelinepostcontainer/AndroidUtils.java
+++ library/src/main/java/com/github/alirezaaa/timelinepostcontainer/AndroidUtils.java
@@ -18,7 +18,6 @@ package com.github.alirezaaa.timelinepostcontainer;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,34 +25,7 @@ import android.view.ViewGroup;
 import com.todddavies.components.progressbar.ProgressWheel;
 import com.wang.avi.AVLoadingIndicatorView;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 final class AndroidUtils {
-    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
-
-    /**
-     * Generate a value suitable for use in {@link View#setId(int)}.
-     * This value will not collide with ID values generated at build time by aapt for R.id.
-     *
-     * @return a generated ID value
-     */
-    static int generateViewId() {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            while (true) {
-                int result = sNextGeneratedId.get();
-                // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
-                int newValue = result + 1;
-                if (newValue > 0x00FFFFFF) {
-                    newValue = 1; // Roll over to 1, not 0.
-                }
-                if (sNextGeneratedId.compareAndSet(result, newValue)) {
-                    return result;
-                }
-            }
-        } else {
-            return View.generateViewId();
-        }
-    }
 
     static ProgressWheel createImageLoading(Context context, ViewGroup viewGroup) {
         return (ProgressWheel) LayoutInflater.from(context).inflate(R.layout.image_loading, viewGroup, false);
