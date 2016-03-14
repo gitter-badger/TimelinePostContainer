9a0bd1f17ad064fb746edcf53bae552ba825533e
Alireza <aesshoferi@gmail.com>
2016-03-14 13:30:50 +0330

    generateViewId() method added

--- library/src/main/java/com/github/alirezaaa/timelinepostcontainer/AndroidUtils.java
+++ library/src/main/java/com/github/alirezaaa/timelinepostcontainer/AndroidUtils.java
@@ -28,7 +28,11 @@ import android.view.ViewGroup;
 import com.todddavies.components.progressbar.ProgressWheel;
 import com.wang.avi.AVLoadingIndicatorView;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 final class AndroidUtils {
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
 
     static ProgressWheel createImageLoading(Context context, ViewGroup viewGroup) {
         return (ProgressWheel) LayoutInflater.from(context).inflate(R.layout.image_loading, viewGroup, false);
@@ -51,5 +55,25 @@ final class AndroidUtils {
             return resources.getDrawable(res, null);
         }
         return resources.getDrawable(res);
+    }
+
+    /**
+     * Generate a value suitable for use in {@link View#setId(int)}.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     *
+     * @return a generated ID value
+     */
+    static int generateViewId() {
+        while (true) {
+            int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) {
+                newValue = 1; // Roll over to 1, not 0.
+            }
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
     }
 }
